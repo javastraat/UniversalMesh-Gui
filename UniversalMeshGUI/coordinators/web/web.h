bc575a9c63f5b3dd192a9fb979b0e0498c4f8f0e
Albert Einstein <javastraat@hotmail.com>
2026-03-28 22:08:26 +0100

    split ini and place code in UNIVERSALMESHGUI

---
+++ UniversalMeshGUI/coordinators/web/web.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <ESPAsyncWebServer.h>
+
+void initWebDashboard(AsyncWebServer& server);
+void logPacket(uint8_t type, uint8_t* senderMac, uint8_t* origSrcMac, uint8_t appId, const uint8_t* payload, uint8_t payloadLen);
+void addSerialLog(const char* msg);
+void lockMeshData();
+void unlockMeshData();
